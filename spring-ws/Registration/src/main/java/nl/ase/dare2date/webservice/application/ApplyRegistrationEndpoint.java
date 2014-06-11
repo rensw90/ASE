336c1aaeea5060d34bb454d680820a96fc6bb733
Rens Weerman <rensw90@gmail.com>
2014-06-11 13:16:11 +0200

    test

--- spring-ws/Registration/src/main/java/nl/ase/dare2date/webservice/application/ApplyRegistrationEndpoint.java
+++ spring-ws/Registration/src/main/java/nl/ase/dare2date/webservice/application/ApplyRegistrationEndpoint.java
@@ -12,6 +12,8 @@ import org.springframework.oxm.Unmarshaller;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 
+import java.io.IOException;
+
 @Endpoint
 public class ApplyRegistrationEndpoint {
 
@@ -19,7 +21,7 @@ public class ApplyRegistrationEndpoint {
     private Unmarshaller unmarshaller;
     private IVerifyRegistration verifyRegistration;
     private IConformRegistration conformRegistration;
-    private ISaveRegistration saveRegistration;
+    //private ISaveRegistration saveRegistration;
     private IVerifyPayment verifyPayment;
 
     public ApplyRegistrationEndpoint(Marshaller marshaller, Unmarshaller unmarshaller, IVerifyRegistration verifyRegistration, IConformRegistration confirmRegistration, ISaveRegistration saveRegistration, IVerifyPayment verifyPayment) {
@@ -27,7 +29,7 @@ public class ApplyRegistrationEndpoint {
         this.unmarshaller = unmarshaller;
         this.verifyRegistration = verifyRegistration;
         this.conformRegistration = confirmRegistration;
-        this.saveRegistration = saveRegistration;
+        //this.saveRegistration = saveRegistration;
         this.verifyPayment = verifyPayment;
     }
 
@@ -36,15 +38,21 @@ public class ApplyRegistrationEndpoint {
     public RegistrationResponse applyRegistration(RegistrationRequest req) {
 
         RegistrationResult result = new RegistrationResult();
+        /*
         if (verifyRegistration.verifyRegistration(req.getInput().getSubscriber()) == true) {
             result.setSucceeded(true);
             //conformRegistration.confirmNewSubscriber(req.getInput().getSubscriber());
         } else {
             result.setSucceeded(false);
-        }
-        saveRegistration.saveSubscriber(req.getInput().getSubscriber());
+        }*/
+        //saveRegistration.saveSubscriber(req.getInput().getSubscriber());
         result.setMessage("Here are the results of the jury for the calculation ");
-        //result.setSucceeded(false);
+        result.setSucceeded(false);
+        try {
+            verifyPayment.verifyPaymentData(req.getInput().getPaymentData());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         RegistrationResponse resp = new RegistrationResponse();
         resp.setResult(result);
 
--- spring-ws/Registration/src/main/java/nl/ase/dare2date/webservice/payment/PayPalAdapter.java
+++ spring-ws/Registration/src/main/java/nl/ase/dare2date/webservice/payment/PayPalAdapter.java
@@ -24,13 +24,23 @@ public class PayPalAdapter implements IMakePayment {
     }
 
     public boolean payment(PaymentData paymentData) throws IOException {
+        /*
         String baseUrl = "https://api.sandbox.paypal.com";
         String urlRequest = "/v1/vault/credit-card";
         String url = baseUrl + urlRequest;
         this.doGet(url);
+        */
+        String token = getPayPalAccessToken();
+        System.out.println(token);
+        System.out.println("TEST");
+        testPayPalPayment();
+
+
+
         return false;
     }
 
+    //Deprecated
     public Boolean doGet(String url) throws IOException {
         /*
         String baseUrl = "https://api.sandbox.paypal.com";
@@ -54,5 +64,41 @@ public class PayPalAdapter implements IMakePayment {
         System.out.println("@@@@@@@@" + test.getString("access_token"));
 
         return true;
+    }
+
+    public String getPayPalAccessToken() {
+        Client client = Client.create();
+        client.addFilter(new HTTPBasicAuthFilter("Acd-OBCht6k0EBUrT0RH-gfSA9pjlWqLyflGslo-qBBEPoIfcp_mrlyHyGk9", "EH7fahDhT6rHE6C-gX3v-sf-zv1pFIgC0kEGbf2B1V7UIDg4c9GBxAk9OvEv"));
+
+        WebResource webResource = client.resource("https://api.sandbox.paypal.com" +
+                "/v1/oauth2/token");
+
+        MultivaluedMapImpl formData = new MultivaluedMapImpl();
+        formData.add("grant_type", "client_credentials");
+
+        ClientResponse clientResponse = webResource.accept(MediaType.APPLICATION_JSON).type(MediaType.APPLICATION_FORM_URLENCODED).post(ClientResponse.class, formData);
+        String output = clientResponse.getEntity(String.class);
+        JSONObject test = new JSONObject(output);
+
+        return test.getString("access_token");
+    }
+
+    public void testPayPalPayment() {
+        Client client = Client.create();
+        client.addFilter(new HTTPBasicAuthFilter("Acd-OBCht6k0EBUrT0RH-gfSA9pjlWqLyflGslo-qBBEPoIfcp_mrlyHyGk9", "EH7fahDhT6rHE6C-gX3v-sf-zv1pFIgC0kEGbf2B1V7UIDg4c9GBxAk9OvEv"));
+
+        WebResource webResource = client.resource("https://api.sandbox.paypal.com" +
+                "/v1/payments/payment");
+
+        MultivaluedMapImpl formData = new MultivaluedMapImpl();
+        formData.add("grant_type", "client_credentials");
+
+        ClientResponse clientResponse = webResource.accept(MediaType.APPLICATION_JSON).type(MediaType.APPLICATION_FORM_URLENCODED).post(ClientResponse.class, formData);
+        String output = clientResponse.getEntity(String.class);
+        JSONObject test = new JSONObject(output);
+
+        System.out.print("Payment Test");
+        System.out.print(test.toString());
+
     }
 }
